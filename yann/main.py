54c153241b2cd7e4dff24be45fda19d1b8fee50c
Yorb <yannberton.yb@gmail.com>
2021-10-30 18:01:51 +0200

    epico

---
+++ yann/main.py
@@ -0,0 +1,12 @@
+# Shift + F10 = Programm Start
+# Strg + F2 = Programm Stoppen
+# Strg + K = Commit
+# Strg + T = Update
+
+# print("Sussy") # Ausgabe
+# input("Wie heißt Deneux: ") # Eingabe
+
+antwort = input("Was ist 2 + 2? ")
+
+if antwort == "4":
+    print("Super! Du kannst voll toll rechnen")
